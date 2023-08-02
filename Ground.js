e1d622c0e6734fa45ee45b7ce72c412ef9d917d7
Dagmara Grabowska <dagmarag0@gmail.com>
2023-08-02 11:17:05 +0200

    Draw ground

--- Ground.js
+++ Ground.js
@@ -1,18 +1,26 @@
-export default class Ground{
-    constructor(ctx, width, height, speed, scaleRatio) {
-        this.ctx = ctx;
-        this.canvas = ctx.canvas;
-        this.width = width;
-        this.height = height;
-        this.speed = speed;
-        this.scaleRatio = scaleRatio;
+export default class Ground {
+  constructor(ctx, width, height, speed, scaleRatio) {
+    this.ctx = ctx;
+    this.canvas = ctx.canvas;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.scaleRatio = scaleRatio;
 
-        this.x = 0;
-        this.y = this.canvas.height - this.height;
+    this.x = 0;
+    this.y = this.canvas.height - this.height;
 
-        this.groundImage = new Image()
-        this.groundImage.src = "./images/ground.png"
+    this.groundImage = new Image();
+    this.groundImage.src = "./images/ground.png";
+  }
 
-    }
-
+  draw() {
+    this.ctx.drawImage(
+      this.groundImage,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
 }
